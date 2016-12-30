d405dfe36137a46346ad0af03e95c904695bf051
QuocLeIT <quoc.le.2107@gmail.com>
2016-12-30 15:52:39 +0700

    Đồ án cuối kỳ web 1

---
+++ includes/left.php
@@ -0,0 +1,34 @@
+
+<p class="TITLE">DANH MỤC</p>
+<div class="left">
+
+
+<div class="text-danhmucsanpham">
+		<?php echo create_menu_Left(); ?> 
+			</div>			
+
+
+<script type="text/javascript">
+var myCountdown2 = new Countdown({
+									time: 1800, 
+									width:235, 
+									height:80, 
+									rangeHi:"hour"	// <- no comma on last item!
+									});
+</script>	
+</div>
+<p class="TITLE">Hỗ trợ</p>
+
+				<div id="hotro">
+			<div class="yahoo"><a href="#"><img src="images/online.gif" alt="" /></a>
+			<p>Phòng Kinh Doanh</p>
+			</div>	
+						
+						<div class="yahoo"><a href="#"><img src="images/online.gif" alt="" /></a>
+			<p>Phòng Kỹ Thuât</p>
+						</div>	
+						<div class="hotline"><img src="images/hotline.jpg" alt="" />
+						<p>09456.98.034</p></div>
+						<div class="hotline"><img src="images/hotline.jpg" alt="" />
+						<p>0966.820.110</p></div>
+</div>
